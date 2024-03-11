0d71e21f3cd40ae560ac95661f832eb3d5ae081e
arjunyadav25d <arjunyadav25d@gmail.com>
2024-03-11 14:27:33 +0530

    project structure added

---
+++ pf-account-service/src/main/java/com/nit/pfaccountservice/PfAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nit.pfaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfAccountServiceApplication.class, args);
+	}
+
+}
---
+++ project-client-allocation-service/project-client-allocation-service/src/test/java/com/nit/projectclientallocationservice/ProjectClientAllocationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit.projectclientallocationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectClientAllocationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
